74c4dcda7a4ad5b294c86ce742e238d0cdbca15d
Isha Goyal <goyalisha03@gmail.com>
2023-09-06 18:00:43 -0400

    setup for warmup project
    includes most of parts 1 and 2 of the project.

---
+++ warmup_project/warmup_project/marker_creator.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+from visualization_msgs.msg import Marker
+
+class ExampleNode(Node):
+    def __init__(self):
+        super().__init__('marker_creator')
+        self.vis_pub = self.create_publisher(Marker, 'visualization_marker', 10)
+
+        timer_period = 10
+        self.timer = self.create_timer(timer_period, self.publish_marker)
+
+def publish_marker(self):
+        marker = Marker()
+        marker.header.frame_id = "base_link";
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.ns = "my_namespace";
+        marker.id = 0
+
+        marker.type = Marker.SPHERE;
+        marker.action = Marker.ADD;
+        marker.pose.position.x = 1.0
+        marker.pose.position.y = 2.0
+        marker.pose.position.z = 1.0
+        marker.scale.x = 1.0
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0; # Don't forget to set the alpha!
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 1.0
+
+        self.vis_pub.publish( marker );
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ExampleNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
